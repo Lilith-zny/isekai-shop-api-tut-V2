afb3c4bc6620626b9b67c48938bf7228b06b4525
Phongsakorn Tangpok <lilith.zny@gmail.com>
2025-12-30 11:47:24 +0700

    echo server

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"github.com/Lilith-zny/isekai-shop-api-tut-V2/config"
+	"github.com/Lilith-zny/isekai-shop-api-tut-V2/databases"
+	"github.com/Lilith-zny/isekai-shop-api-tut-V2/server"
+)
+
+func main() {
+	conf := config.ConfigGetting()
+	db := databases.NewPostgresDatabase(conf.Database)
+	server := server.NewEchoServer(conf, db.ConnectionGetting())
+
+	server.Start()
+}
