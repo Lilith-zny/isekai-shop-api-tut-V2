6905017b50604ded5f1facd2c59287278d4f646f
Phongsakorn Tangpok <lilith.zny@gmail.com>
2025-12-30 15:56:12 +0700

    pagination

--- pkg/itemShop/repository/itemShopRepositoryImpl.go
+++ pkg/itemShop/repository/itemShopRepositoryImpl.go
@@ -20,7 +20,7 @@ func NewItemShopRepositoryImpl(db *gorm.DB, logger echo.Logger) ItemShopReposito
 func (r *itemShopRepositoryImpl) Listing(itemFilter *_itemShopModel.ItemFilter) ([]*entities.Item, error) {
 	itemList := make([]*entities.Item, 0)
 
-	query := r.db.Model(&entities.Item{}) // select * from items
+	query := r.db.Model(&entities.Item{}).Where("is_archive = ?", false) // select * from items
 
 	if itemFilter.Name != "" {
 		query = query.Where("name ilike ?", "%"+itemFilter.Name+"%")
@@ -30,9 +30,39 @@ func (r *itemShopRepositoryImpl) Listing(itemFilter *_itemShopModel.ItemFilter) 
 		query = query.Where("description ilike ?", "%"+itemFilter.Description+"%")
 	}
 
-	if err := query.Find(&itemList).Error; err != nil {
+	// offset
+	// 1 2 3 4 5 6 7 8 9 10
+	// 0       | 5       |
+
+	// (page - 1) * size
+	// (1 - 1) * 5 = 0
+	// (2 - 1) * 5 = 5
+	offset := int((itemFilter.Page - 1) * itemFilter.Size)
+	limit := int(itemFilter.Size)
+
+	if err := query.Offset(offset).Limit(limit).Find(&itemList).Order("id asc").Error; err != nil {
 		r.logger.Errorf("Failed to list items: %s", err.Error())
 		return nil, &_itemShopException.ItemListing{}
 	}
 	return itemList, nil
+}
+
+func (r *itemShopRepositoryImpl) Counting(itemFilter *_itemShopModel.ItemFilter) (int64, error) {
+	query := r.db.Model(&entities.Item{}).Where("is_archive = ?", false) // select * from items
+
+	if itemFilter.Name != "" {
+		query = query.Where("name ilike ?", "%"+itemFilter.Name+"%")
+	}
+
+	if itemFilter.Description != "" {
+		query = query.Where("description ilike ?", "%"+itemFilter.Description+"%")
+	}
+
+	var count int64
+
+	if err := query.Count(&count).Error; err != nil {
+		r.logger.Errorf("Counting items failed: %s", err.Error())
+		return -1, &_itemShopException.ItemCounting{}
+	}
+	return count, nil
 }
--- pkg/itemShop/service/itemShopServiceImpl.go
+++ pkg/itemShop/service/itemShopServiceImpl.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"github.com/Lilith-zny/isekai-shop-api-tut-V2/entities"
 	_itemShopModel "github.com/Lilith-zny/isekai-shop-api-tut-V2/pkg/itemShop/model"
 	_itemShopRepository "github.com/Lilith-zny/isekai-shop-api-tut-V2/pkg/itemShop/repository"
 )
@@ -13,18 +14,44 @@ func NewItemShopServicImpl(itemShopRepository _itemShopRepository.ItemShopReposi
 	return &itemShopServiceImpl{itemShopRepository}
 }
 
-func (s *itemShopServiceImpl) Listing(itemFilter *_itemShopModel.ItemFilter) ([]*_itemShopModel.Item, error) {
+func (s *itemShopServiceImpl) Listing(itemFilter *_itemShopModel.ItemFilter) (*_itemShopModel.ItemResult, error) {
 	// entity
 	itemList, err := s.itemShopRepository.Listing(itemFilter)
 	if err != nil {
 		return nil, err
 	}
 
-	// model
+	itemCounting, err := s.itemShopRepository.Counting(itemFilter)
+	if err != nil {
+		return nil, err
+	}
+
+	totalPage := s.totalPageCalculation(itemCounting, itemFilter.Size)
+
+	return s.toItemResultResponse(itemList, itemFilter.Page, totalPage), nil
+}
+
+func (s *itemShopServiceImpl) totalPageCalculation(totalItem int64, size int64) int64 {
+	totalPage := totalItem / size
+
+	if totalItem%size != 0 {
+		totalPage++
+	}
+
+	return totalPage
+}
+
+func (s *itemShopServiceImpl) toItemResultResponse(itemEntityList []*entities.Item, page, totalPage int64) *_itemShopModel.ItemResult {
 	itemModelList := make([]*_itemShopModel.Item, 0)
-	for _, item := range itemList {
+	for _, item := range itemEntityList {
 		itemModelList = append(itemModelList, item.ToItemModel())
 	}
 
-	return itemModelList, nil
+	return &_itemShopModel.ItemResult{
+		Items: itemModelList,
+		Paginate: _itemShopModel.PaginateResult{
+			Page:      page,
+			TotalPage: totalPage,
+		},
+	}
 }
